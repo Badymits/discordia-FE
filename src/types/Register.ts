6d7e6edec46227532b42b29e83fb7acadd5a8a07
Elijah Cajucom <elijah.cajucom@gmail.com>
2026-05-08 02:35:18 +0800

    fix: delete file, transferred to User.ts file

--- src/types/Register.ts
+++
@@ -1,9 +0,0 @@
-export interface RegisterForm {
-    firstname: string;
-    lastname: string;
-    email: string
-    userName: string;
-    displayName: string;
-    password: string
-    confirmPassword?: string
-}
