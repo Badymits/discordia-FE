3d701eed2d11c9a1bf7b2d123eda065da43bc4d2
Elijah Cajucom <elijah.cajucom@gmail.com>
2026-05-10 09:42:08 +0800

    feat: add query to update to fetch existing DM's

--- src/components/DmAndChannelComp.tsx
+++ src/components/DmAndChannelComp.tsx
@@ -3,14 +3,48 @@ import { IoCloseCircleOutline } from 'react-icons/io5';
 import { Link } from 'react-router-dom';
 import { useServerContext } from '../context/ServerContext';
 import type {
+  DirectChannel,
   ServerMembers
 } from "../types/ServerTypes";
+import { useState } from 'react';
+import type { ServerUser } from '../types/User';
+import { useQuery } from '@tanstack/react-query';
+import { fetchDirectChannels } from '../query/serverQueries';
+import { BsDiscord } from 'react-icons/bs';
 
 
 const DmAndChannelComp = () => {
 
-
   const {sampleUsers}  = useServerContext();
+
+  const [currentUser] = useState<ServerUser>(() => {
+  
+    const userObj = JSON.parse(sessionStorage.getItem("UserObj") || "")
+
+    if (userObj) return {
+      userId: userObj.userId,
+      displayName: userObj.displayname,
+      username: userObj.username,
+      imgUrl: userObj.imgUrl,
+    }
+
+    return {} as ServerUser
+  })
+
+  const { data: directChannels, isLoading } = useQuery({
+    ...fetchDirectChannels(currentUser.userId || ""),
+    enabled: !!currentUser.userId,
+    staleTime: 1000 * 10 * 60,
+    gcTime: 1000 * 10 * 60
+  })
+
+  console.log(directChannels)
+
+  if (isLoading){
+    return (
+      <div>We'll be back</div>
+    )
+  }
 
 
   return (
@@ -28,6 +62,61 @@ const DmAndChannelComp = () => {
 
       <div className='overflow-y-auto h-210 scrollbar-thin
       scrollbar-thumb-[#99AAB5] scrollbar-track-transparent'>
+
+        {
+          directChannels?.data?.map((dc: DirectChannel, i:number) => {
+
+            // since its guaranteed that there 2 users, just filter excluding current user ID and
+            // just get that user
+            const recipient: ServerUser = 
+                dc?.directChannelParticipants?.filter((user) => user.userId !== currentUser.userId)[0]
+
+            return (
+              <Link
+                to={`/messages/${dc.directChannelId}`}
+                key={i}
+                className='flex items-center justify-between p-2 m-1  
+                  rounded-md cursor-pointer hover:bg-[#444649] truncate'
+                >
+                <div className='flex items-center justify-center gap-2 '>
+                    <div className='bg-gray-400 h-10 w-10 rounded-full relative'>
+                      <div>
+                        {
+                          recipient.imgUrl ?
+                          <img 
+                            src={recipient.imgUrl}
+                            alt={recipient.displayName}
+                            className='h-full w-full rounded-full object-cover'
+                            />
+                          : <BsDiscord className='h-full w-full bg-indigo-500 rounded-full p-1'/>
+                        }
+                      </div>
+                      
+                      
+                      {/* <div className={`
+                        ${user.status == "online" 
+                          ? "bg-green-700" 
+                          : user.status == "idle" 
+                            ? "bg-yellow-500"
+                            : user.status == "away"
+                              ? "bg-red-700"
+                          : "bg-gray-500"
+                        } h-3 w-3 absolute 
+                        right-0 bottom-0 rounded-full`}>
+                      
+                      </div> */}
+                  </div>
+
+                  <div className='truncate w-30'>
+                    <p className='font-semibold'>{recipient.displayName}</p>
+                    <p className='text-sm text-gray-400'>{recipient.username}</p>
+                  </div>
+                </div>
+              </Link>
+            )
+            
+          })
+        }
         {sampleUsers.map((user:ServerMembers, i:number) => (
           <Link 
             to={`/messages/${user.userTag}`}
