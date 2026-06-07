6d3a394ca3d7692a6c1ce58bea35514e3f61b3ac
Elijah Cajucom <elijah.cajucom@gmail.com>
2026-06-08 01:10:52 +0800

    feat: add patch methods for message. add server id as request param.

--- src/services/serverService.ts
+++ src/services/serverService.ts
@@ -26,12 +26,12 @@ export const updateServer = async(serverData: FormData, serverId: string) => {
   })
 }
 
-export const updateChannel = async(channelData: UpdateChannelPayload, channelId: string) => {
-  return http.post(`/channels/update/${channelId}`, channelData)
+export const updateChannel = async(channelData: UpdateChannelPayload, channelId: string, serverId: string) => {
+  return http.post(`/channels/update/${channelId}?serverId=${serverId}`, channelData)
 }
 
-export const updateCategory = async(categoryName: string, categoryId: string) => {
-  return http.post(`/category/update/${categoryId}`, categoryName)
+export const updateCategory = async(categoryName: string, categoryId: string, serverId: string) => {
+  return http.post(`/category/update/${categoryId}?serverId=${serverId}`, categoryName)
 }
 
 export const createServer = async (serverData: FormData) => {
@@ -42,26 +42,69 @@ export const createServer = async (serverData: FormData) => {
   })
 }
 
-export const createChannel = async ({categoryId, channelName, icon}: CreateChannelPayload) => {
-  return http.post('/channels/create-channel', {categoryId, channelName, icon})
+export const createChannel = async ({categoryId, channelName, icon, serverId}: CreateChannelPayload) => {
+  return http.post(`/channels/create-channel?serverId=${serverId}`, {categoryId, channelName, icon})
 }
 
 export const createCategory = async({serverId, categoryName}: CreateCategoryPayload) => {
   return http.post('/category/create-category', {serverId, categoryName})
 }
 
-export const uploadImage = async(formData: FormData, messageId: string) => {
-  return http.post(`/server-messages/upload-image/${messageId}`, formData, {
+export const uploadImage = async(formData: FormData, messageId: string, messageType: string) => {
+
+  if (messageType === "server"){
+    return http.post(`/server-messages/upload-image/${messageId}`, formData, {
+      headers: {
+        "Content-Type": "multipart/form-data"
+      }
+    }).then(res => res.data)
+
+  } else if (messageType === "direct"){
+    return http.post(`/direct-messages/upload-image/${messageId}`, formData, {
     headers: {
       "Content-Type": "multipart/form-data"
     }
   }).then(res => res.data)
+  }
 }
 
-export const createDirectChannel = async(participants: ServerUser[]) => {
+export const createDirectChannel = async(participants: ServerUser[], userId: string) => {
   return http.post(`/direct-channel/create-direct-channel`, {
-    directChannelParticipants: participants
+    directChannelParticipants: participants,
+    userId: userId
   })
+}
+// =========================
+
+
+// ====== PATCH METHODS ======
+
+export const updateMessage = async(
+  channelId: string,
+  messageId: string, 
+  message:string,
+  messageType: string
+) => {
+
+  let updateUrl = "";
+
+  if (messageType === "server"){
+    updateUrl = `/server-messages/update-server-message/${messageId}?channelId=${channelId}`
+  } else if (messageType === "direct"){
+    updateUrl = `/direct-messages/update-direct-message/${messageId}`
+  }
+
+  return http.patch(updateUrl, message,
+    {
+      headers: {
+        "Content-Type": "text/plain"
+      }
+    }
+  ).then(res => res.data)
+}
+
+export const updateReadMessages = async (directChannelId: string) => {
+  return http.patch(`/direct-channel/update-read-messages/${directChannelId}`)
 }
 
 // =========================
@@ -93,6 +136,10 @@ export const getServersByUserId = async(userId: string | number) => {
   return http.get(`/server/list/${userId}`)
 }
 
+export const getServerMetaData = async(serverId: string) => {
+  return http.get(`/server/get-metadata/${serverId}`)
+}
+
 export const getChannelById = async(channelId: string) => {
   return http.get(`/channels/${channelId}`)
 }
@@ -101,8 +148,16 @@ export const getServerMessagesByChannelId = async(channelId: string | number) =>
   return http.get(`/server-messages/${channelId}`)
 }
 
-export const getDirectChannels = async(userId: string) => {
+export const getDirectChannels = async (userId: string) => {
   return http.get(`/direct-channel/get-direct-channels/${userId}`)
+}
+
+export const getDirectChannel = async(directChannelId: string) => {
+  return http.get(`/direct-channel/get-direct-channel/${directChannelId}`)
+}
+
+export const getDirectMessages = async(directChannelId: string) => {
+  return http.get(`/direct-messages/get-direct-messages/${directChannelId}`)
 }
 
 // =========================
@@ -110,16 +165,20 @@ export const getDirectChannels = async(userId: string) => {
 
 // ====== DELETE METHODS ======
 
-export const deleteChannel = async(channelId: string) => {
-  return http.delete(`/channels/${channelId}`)
+export const deleteChannel = async(channelId: string, serverId: string) => {
+  return http.delete(`/channels/${channelId}?serverId=${serverId}`)
 }
 
-export const deleteCategory = async(categoryId: string) => {
-  return http.delete(`/category/${categoryId}`)
+export const deleteCategory = async(categoryId: string, serverId: string) => {
+  return http.delete(`/category/${categoryId}?serverId=${serverId}`)
 }
 
-export const deleteServerMessage = async(messageId: string) => {
-  return http.delete(`/server-messages/${messageId}`)
+export const deleteServerMessage = async(channelId:string, messageId: string, serverId: string) => {
+  return http.delete(`/server-messages/${serverId}/${channelId}/${messageId}`)
+}
+
+export const deleteDirectMessage = async(directChannelId: string, messageId: string) => {
+  return http.delete(`/direct-messages/${directChannelId}/${messageId}`)
 }
 
 // ============================
