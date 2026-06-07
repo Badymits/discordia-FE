1c7d3442619e19ce33939595975ebbafb5c3f7fe
Elijah Cajucom <elijah.cajucom@gmail.com>
2026-06-08 01:05:21 +0800

    feat: add notification query. add update read messages mutate. add use effect subsribe to listen to notification pop ups

--- src/components/ServerList.tsx
+++ src/components/ServerList.tsx
@@ -1,17 +1,20 @@
-
+import { useEffect } from 'react';
 import { Link, useNavigate, useParams } from 'react-router-dom'
 import { Tooltip } from 'react-tooltip'
-import {  useServerContext } from '../context/ServerContext';
+import { useServerContext } from '../context/ServerContext';
 
+import type { NotificationPayload, Server } from '../types/ServerTypes';
+
+import { fetchServersList } from '../query/serverQueries';
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
+import { useCurrentUser } from '../context/UserContext';
+import ServerListLoader from './LoadingComponents/ServerListLoader';
+import { useWebSocketContext } from '../context/WebSocketContext';
 
 import { BiPlusCircle } from 'react-icons/bi'
 import { BsDiscord } from 'react-icons/bs'
-import type { Server } from '../types/ServerTypes';
+import { updateReadMessages } from '../services/serverService';
 
-import { fetchServersList } from '../query/serverQueries';
-import { useQuery } from '@tanstack/react-query';
-import { useCurrentUser } from '../context/UserContext';
-import ServerListLoader from './LoadingComponents/ServerListLoader';
 
 interface ServerListProps{
   serverList: Server[];
@@ -24,13 +27,23 @@ const ServerList = (
     handleFunction
 }: ServerListProps) => {
 
-  const { serverId } = useParams()
+  const queryClient = useQueryClient();
+  const { serverId, directChannelId} = useParams()
   const { user } = useCurrentUser()
   const {
     setServer
   } = useServerContext();
 
+  const {
+    subscribe,
+    unsubscribe,
+    socketConnection,
+    isConnected
+  } = useWebSocketContext();
+
   const navigate = useNavigate()
+
+  //const [directMessageNotifs, setDirectMessagesNotifs] = useState<NotificationPayload[] | undefined>(undefined)
 
   console.log(user)
 
@@ -39,6 +52,61 @@ const ServerList = (
     enabled: !!user?.userId, // Prevents fetching data until UserID has value
     staleTime: 1000 * 5 * 60,
     gcTime: 1000 * 5 * 60
+  })
+
+  const { 
+    data: notifications, 
+    isLoading: isNotificationLoading 
+  } = useQuery<NotificationPayload[]>({
+    queryKey: ["notifications-messages"],
+    queryFn: () => {return []},
+    enabled: !!socketConnection,
+    staleTime: 1000 * 5 * 60,
+    gcTime: 1000 * 5 * 60
+  })
+
+  const {
+    mutate: updateReadMessagesMutate
+  } = useMutation({
+    mutationKey: ["updateReadMessages"],
+    mutationFn: async (channelId: string) => await updateReadMessages(channelId || ""),
+
+    onMutate: async (channelId) => {
+      await queryClient.cancelQueries({queryKey: ["notifications-messages"]})
+
+      const notifications = queryClient.getQueryData(["notifications-messages"])
+
+      if (!notifications) return;
+
+      queryClient.setQueryData(["notifications-messages"], (notifs: NotificationPayload[]) => {
+        if (!notifs) return [];
+
+        return notifs.filter(n => n.directChannelId !== channelId)
+      })
+
+      return { notifications, channelId }
+    },
+
+    onError: (error, _variables, context) => {
+      console.error("Error: ", error);
+      queryClient.setQueryData(["notifications-messages"], context?.notifications)
+    },
+
+    onSuccess: (response, _variables, context) => {
+
+      if (!response){
+        console.error("Data not found");
+        return;
+      }
+
+      queryClient.setQueryData(["notifications-messages"], (notifs: NotificationPayload[]) => {
+        if (!notifs) return [];
+
+        return notifs.filter(n => n.directChannelId !== context?.channelId)
+      })
+
+      console.log(queryClient.getQueryData(["notifications-messages"]))
+    }
   })
 
   const serverList: Server[] = data?.data || []
@@ -71,25 +139,142 @@ const ServerList = (
       .map(word => word.charAt(0).toUpperCase())
       .join('');
   }
+  console.log(socketConnection , isConnected)
+
+  const updateIsReadChannel = (channelId: string) => {
+    console.log("Changing...")
+
+    if (!channelId || !user){
+      console.error("Channeld ID and/or User not found!")
+      return;
+    }
+
+    console.log("Removing notification....")
+
+    updateReadMessagesMutate(channelId)
+  }
 
 
-  if (isLoading) {
+  useEffect(() => {
+
+    // notification append will not run under these conditions
+    if (!user || !isConnected || !socketConnection || !!directChannelId){
+      return;
+    }
+
+    if (socketConnection && isConnected && !directChannelId){
+
+      const timer = setTimeout(() => {
+        subscribe(`/direct/${user.userId}`, (notificationPayload: NotificationPayload) => {
+        console.log(notificationPayload)
+
+        const userId = notificationPayload.userId
+        const channelId = notificationPayload.directChannelId;
+
+        if (!channelId|| !userId){
+          console.error("User ID and/or DirectChannelId not detected!")
+          return;
+        }
+
+        if (!notificationPayload) return;
+
+        queryClient.setQueryData(["notifications-messages"], (notifs: NotificationPayload[]) => {
+          if (notifs === null || notifs === undefined) return [];
+
+          const targetedRoom = notifs.find(room => room.directChannelId === channelId)
+        
+
+          if (!targetedRoom) return [...notifs, notificationPayload]
+
+          return notifs.map(
+            n => n.directChannelId === channelId
+            ? {...n, unreadMessages: notificationPayload.unreadMessages}
+            : n
+          )
+        })
+      })
+
+      }, 100)
+      
+      return () => clearTimeout(timer)
+    }
+
+    return () => {
+      if (socketConnection && isConnected){
+        unsubscribe(`/direct/${user.userId}`)
+      }
+    }
+
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [
+    subscribe, 
+    unsubscribe,
+    directChannelId,
+    socketConnection,
+    isConnected,
+    user
+  ])
+
+
+  if (isLoading || isNotificationLoading) {
     return <ServerListLoader />
   }
     
-
   return (
     <div className='bg-[#111113] w-19.5 h-full px-2 shrink-0'>
     
       <div className='w-full flex flex-col items-center'> 
+      
+        {
+          (notifications || []).map((notif: NotificationPayload, i) => (
+            <Link
+              to={`/messages/${notif?.directChannelId}`}
+              key={i}
+              onClick={() => updateIsReadChannel(notif.directChannelId)}
+              data-tooltip-id="direct-message-notif"
+              data-tooltip-content={notif?.displayName}
+              className='relative'
+            >
+              {
+                notif?.userAvatar 
+                ? <img 
+                    src={notif?.userAvatar}
+                    alt={notif?.displayName}
+                    className='h-11 w-11.5 rounded-md mt-3'
+                  />
+                : <BsDiscord 
+                  className='mt-3 h-11 w-11.5 p-2 text-white
+                    cursor-pointer rounded-xl bg-[#23272a] hover:bg-indigo-500'   
+                  />
+              }
+              <p className='bg-red-500 text-white w-4 text-center 
+                text-xs rounded-full absolute -bottom-1 -right-1 font-bold'>
+                {notif.unreadMessages}
+              </p>
+              <Tooltip id='direct-message-notif' place='right' className='z-99'/>
+              
+            </Link>
+          ))
+        }
         {/* Home Btn */}
-        <Link to="/">
-          <BsDiscord className='
-          text-4xl text-[#7289DA] my-3 h-12 w-12.5 p-2 
-          cursor-pointer rounded-xl bg-[#23272a] hover:bg-[#7289DA] hover:text-white'/>
+        <Link to="/" className='relative group' data-tooltip-id='direct-messages' data-tooltip-content="Direct Messages">
+          <BsDiscord className={`
+          text-4xl text-[#7289DA] my-3 h-11 w-11.5 p-2 
+          cursor-pointer rounded-xl bg-[#23272a] hover:bg-indigo-500 hover:text-white
+          ${!serverId && "bg-indigo-500 text-white"}
+          `}
+          />
+          
+          {/* UI to indicate where user is (in this case in direct messages page) */}
+          {
+            !serverId && <div className={`absolute -left-4 w-1 top-6.75 bg-white rounded-r-full 
+              transition-all duration-300 
+              h-0 group-hover:h-5 ${!serverId ? "h-5 w-1.5" : ""}`}></div>
+          }
+          <Tooltip id='direct-messages' place='right' className='z-50 font-bold'/>
         </Link>
 
-        <hr className='border border-gray-400 w-12.5'/>
+        <hr className='border border-gray-400 w-11.5'/>
         {serverList!.map((server: Server, i: number) => (
           <button key={i}
             
@@ -107,7 +292,7 @@ const ServerList = (
             }}
             data-tooltip-id="my-server-tooltip" // Shared ID
             data-tooltip-content={server.serverName}
-            className='h-13 w-13  flex 
+            className='h-11 w-11.5  flex 
             cursor-pointer items-center justify-center rounded-xl my-2 duration-200`
             relative group opacity-100
             '>
@@ -152,7 +337,7 @@ const ServerList = (
           type='button'
           data-tooltip-id="my-server-tooltip" 
           data-tooltip-content="Create Server"
-          className='h-13.5 w-12.5 bg-white hover:bg-[#7289DA] flex 
+          className='h-11 w-11.5 bg-white hover:bg-indigo-500 flex 
             cursor-pointer items-center justify-center rounded-xl my-2 duration-200`
             relative group opacity-100'
             onClick={() => handleFunction()}
@@ -161,7 +346,7 @@ const ServerList = (
           duration-100 text-gray-500'/> 
         </button>
 
-        <Tooltip id="my-server-tooltip" place='right' className='z-50'/>
+        <Tooltip id="my-server-tooltip" place='right' className='z-50 font-bold opacity-100 bg-black'/>
       </div>
     </div>
   )
